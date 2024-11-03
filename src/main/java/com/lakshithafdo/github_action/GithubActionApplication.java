0b97894065337f669b0c52895b20433d64064cc2
lakshitha200 <lakshithaf20@gmail.com>
2024-11-03 02:43:11 -0800

    New Files added

--- src/main/java/com/lakshithafdo/github_action/GithubActionApplication.java
+++ src/main/java/com/lakshithafdo/github_action/GithubActionApplication.java
@@ -7,17 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/api/welcome")
 public class GithubActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello GitHub Action";
-	}
 
 }
--- src/test/java/com/lakshithafdo/github_action/GithubActionApplicationTests.java
+++ src/test/java/com/lakshithafdo/github_action/GithubActionApplicationTests.java
@@ -1,13 +1,36 @@
 package com.lakshithafdo.github_action;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(HelloController.class)
 class GithubActionApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private HelloService helloService;
+
+
 	@Test
-	void contextLoads() {
+	public void helloControllerTest() throws Exception{
+
+		ResultActions response = mockMvc.perform(get("/api/hello"));
+		response.andExpect(status().isOk())
+				.andDo(print());
+
 	}
 
 }
